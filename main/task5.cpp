cacc57d1e2dd674c778411b9129d60397a8975f3
Ash <98084216+ashar933@users.noreply.github.com>
2024-03-06 19:06:50 +0530

    Update task5.cpp

--- main/task5.cpp
+++ main/task5.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main(void) {
     cout << "Task 5" << endl;
-    cout << "Jenkins pipeline works!" << endl
+    cout << "Jenkins pipeline works!" << endl;
     return 0;
 }
